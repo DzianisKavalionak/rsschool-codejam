8ec342a9b09493d5a67455d3812ec063b09b66ae
DzianisKavalionak <nereidy4@gmail.com>
2018-10-28 19:46:12 +0300

    feat : add recursion

--- tasks/recursion.js
+++ tasks/recursion.js
@@ -2,17 +2,13 @@ module.exports = function recursion(tree) {
     let result = [];
     traverseLevels(tree, result, 0);
     return result;
-
-
-
-    function traverseLevels(tree, arr, level) {
-        if ("value" in tree) {
-            if (typeof(arr[level]) === "undefined") arr[level] = [];
-            arr[level].push(tree["value"]);
-        }
-        if ("left" in tree) traverseLevels(tree["left"], arr, level+1);
-        if ("right" in tree) traverseLevels(tree["right"], arr, level+1);
-    }
-
-
 };
+
+function traverseLevels(tree, arr, level) {
+    if ("value" in tree) {
+        if (typeof(arr[level]) === "undefined") arr[level] = [];
+        arr[level].push(tree["value"]);
+    }
+    if ("left" in tree) traverseLevels(tree["left"], arr, level+1);
+    if ("right" in tree) traverseLevels(tree["right"], arr, level+1);
+}
--- test3.js
+++ test3.js
@@ -5,8 +5,8 @@ describe('make', () => {
     it('partialFunctionSumTest', () => {
         const tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
         let array = recursion(tree);
-        const result = [[100], [90, 120], [70,99,110,130]];
-        assert.deepEqual(array, result);
+        const res = [[100], [90, 120], [70,99,110,130]];
+        assert.deepEqual(array, res);
     });
 
 });
