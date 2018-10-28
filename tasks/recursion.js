fae6169fea6d37460c7120815e06de9ec70cab17
DzianisKavalionak <nereidy4@gmail.com>
2018-10-28 19:37:10 +0300

    feat: add recursion

---
+++ tasks/recursion.js
@@ -0,0 +1,18 @@
+module.exports = function recursion(tree) {
+    let result = [];
+    traverseLevels(tree, result, 0);
+    return result;
+
+
+
+    function traverseLevels(tree, arr, level) {
+        if ("value" in tree) {
+            if (typeof(arr[level]) === "undefined") arr[level] = [];
+            arr[level].push(tree["value"]);
+        }
+        if ("left" in tree) traverseLevels(tree["left"], arr, level+1);
+        if ("right" in tree) traverseLevels(tree["right"], arr, level+1);
+    }
+
+
+};
